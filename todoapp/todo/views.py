27b179c33558b3a5d0a97214a8a048cf4cb11e8a
Chif Gergő <chifgeri97@gmail.com>
2020-06-22 18:12:47 +0200

    Add view and template for list todos

--- todoapp/todo/views.py
+++ todoapp/todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import TodoItem
 
 # Create your views here.
+class TodoListView(ListView):
+    model = TodoItem
+    template_name = "todo/todo_list.html"
+    context_object_name = 'todo_list'
+    
+    def get_queryset(self):
+        return super().get_queryset()
+    
