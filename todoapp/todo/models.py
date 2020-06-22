66c1731654e2ba5542af6b53cd6274ed65c0fa82
Chif Gergő <chifgeri97@gmail.com>
2020-06-22 13:33:32 +0200

    Crate model for todos

--- todoapp/todo/models.py
+++ todoapp/todo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class TodoItem(models.Model):
+    text = models.CharField(blank=False, null=False, max_length=100)
+    done = models.BooleanField(default=False)
+    priority = models.PositiveSmallIntegerField()
+    
+
+    class Meta:
+        verbose_name = _("TodoItem")
+        verbose_name_plural = _("TodoItems")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("todoitem_detail", kwargs={"pk": self.pk})
