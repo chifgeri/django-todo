b0a742b7971cfc44eb5b064ea5edcaf5293cccf6
Chif Gergő <chifgeri97@gmail.com>
2020-06-22 13:48:08 +0200

    Fix some error, register in admin

--- todoapp/todo/models.py
+++ todoapp/todo/models.py
@@ -8,11 +8,17 @@ class TodoItem(models.Model):
     
 
     class Meta:
-        verbose_name = _("TodoItem")
-        verbose_name_plural = _("TodoItems")
+        verbose_name = "TodoItem"
+        verbose_name_plural = "TodoItems"
 
     def __str__(self):
-        return self.name
+        return self.text
 
     def get_absolute_url(self):
         return reverse("todoitem_detail", kwargs={"pk": self.pk})
+
+    def check_todo(self):
+      self.done = True
+      self.priority = 0
+      self.save()
+
