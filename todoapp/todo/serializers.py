0d8774735aed308541e5dae884ad6a509a4e3611
Chif Gergő <chifgeri97@gmail.com>
2020-07-01 11:15:39 +0200

    Make done, and priotity read only

--- todoapp/todo/serializers.py
+++ todoapp/todo/serializers.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import TodoItem
 
-class TodoSerializer(serializers.ModelSerializer):
+
+class TodoItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = TodoItem
-        fields = __all__
+        fields = '__all__'
+        read_only_fields = ('id', 'priority', 'done')
